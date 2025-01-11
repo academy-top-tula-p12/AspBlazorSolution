c3997bd850310d9d09b950e998aa2cc58a35db1e
Maxim Efimov <efimov.top.academy@gmail.com>
2025-01-11 17:40:13 +0300

    Добавьте файлы проекта.

---
+++ AspBlazorApp/Program.cs
@@ -0,0 +1,16 @@
+using AspBlazorApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+
+var app = builder.Build();
+
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+   .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ BlazorWebAssemblyApp/BlazorWebAssemblyApp/Program.cs
@@ -0,0 +1,21 @@
+using BlazorWebAssemblyApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorWebAssemblyApp.Client._Imports).Assembly);
+
+app.Run();
