c36fd36f7c268747d3f2c7f575175d15bc82a001
Maxim Efimov <efimov.top.academy@gmail.com>
2025-01-21 19:54:22 +0300

    21-01-25

--- BlazorFormsApp/Models/Employee.cs
+++ BlazorFormsApp/Models/Employee.cs
@@ -1,12 +1,36 @@
-﻿namespace BlazorFormsApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorFormsApp.Models
 {
     public class Employee
     {
+        [Required(ErrorMessage = "Имя обязательно для заполнения")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Длина имени от {2} до {1} символов")]
         public string Name { get; set; } = "";
         public DateTime BirthDate { get; set; }
+
+        [Range(17000, 1000000, ErrorMessage = "Размер зарплаты от {1} до {2} рублей")]
         public int Salary { get; set; }
         public bool Married { get; set; }
+
+        [EmployeeGenderValidator(new[] {"Male", "Female"})]
         public string Gender { get; set; } = "";
         public string[] Skills { get; set; } = [];
+    }
+
+    public class EmployeeGenderValidator : ValidationAttribute
+    {
+        string[] genders;
+        public EmployeeGenderValidator(string[] genders)
+        {
+            this.genders = genders;
+        }
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (genders.Contains(value?.ToString()))
+                return ValidationResult.Success;
+            else
+                return new ValidationResult("Некооректный гендер");
+        }
     }
 }
